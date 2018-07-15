b98a7b8359898c0c8a549bbf8be941bba431ecf4
khashinik <khashinik@gmail.com>
2018-07-14 22:54:53 -0400

    added

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+var userData = [
+    {
+       "name":"Paul",
+       "photo":"https://assets.change.org/photos/9/ua/hs/zuUAHSOETXZYLTS-800x450-noPad.jpg?1493470060",
+       "scores":[1,1,2,4,3,1,2,5,4,1]
+    },
+    {
+       "name":"Ryan",
+       "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYvvwhLkPTsylBnebpF7iNsH1cbTEQ7yrtl3ili3gDCg3C8qyN",
+       "scores":[6,2,5,1,3,2,2,1,3,3]
+    },
+    {
+       "name":"Jimmy",
+       "photo":"https://img.discogs.com/Cr86hmmdfMwbY0MqKrbPggeWnJo=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/A-28365-1510618149-2715.jpeg.jpg",
+       "scores":[5,2,2,2,4,1,3,2,5,2]
+    },
+    {
+       "name":"Shane",
+       "photo":"http://www.onlineworldofwrestling.com/pictures/s/shanechung/01.jpg",
+       "scores":[4,3,3,2,1,4,3,2,2,3]
+    }
+]
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,72 @@
+var userData = require("../data/friends");
+
+
+module.exports = function(app) {
+
+  app.get("/api/friends", function(req, res) {
+    res.json(userData);
+  });
+
+  var comparisonUserTotalScore = 0;
+
+  var friendScores = [];
+
+  app.post("/api/friends", function(req, res) {
+
+
+    var currentUserScores = req.body.scores;
+
+    console.log("Current user scores: " + currentUserScores);
+
+ 
+    for (var i = 0; i < userData.length; i++) {
+
+  
+      var comparisonUserScores = userData[i].scores;
+
+     
+      comparisonUserTotalScore = calculateUserCompatibilityScore(currentUserScores, comparisonUserScores);
+
+      friendScores.push(comparisonUserTotalScore);
+
+    }
+
+    console.log("Array of friend scores: " + friendScores);
+
+    var index = 0;
+    var value = friendScores[0];
+
+
+    for (var i = 0; i < friendScores.length; i++) {
+      if (friendScores[i] < value) {
+        value = friendScores[i];
+        index = i;
+      }
+    }
+
+
+
+ 
+    res.send(userData[index]);
+
+
+  });
+};
+
+var totalDifference = 0;
+
+
+function calculateUserCompatibilityScore(currentUserScores, comparisonUserScores) {
+
+
+  totalDifference = 0;
+
+  for (var i = 0; i < currentUserScores.length; i++) {
+
+    totalDifference+=Math.abs(currentUserScores[i] - comparisonUserScores[i]);
+  }
+
+  console.log("Final total difference for friend: " + totalDifference);
+
+  return totalDifference;
+};
